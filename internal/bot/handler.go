b99be23898d4ca1276c8a730daf18b4432250a43
ogrock3t <abubakirov.offical@gmail.com>
2025-04-25 12:57:26 +0300

    Added functuonality

--- internal/bot/handler.go
+++ internal/bot/handler.go
@@ -3,8 +3,15 @@ package bot
 import (
 	"fmt"
 	"log"
+	"sync"
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+)
+
+var (
+	lastUserMsgID int
+	lastBotMsgID  int
+	msgMutex      sync.Mutex
 )
 
 func Run(token string) {
@@ -43,7 +50,9 @@ func handleMessage(botAPI *tgbotapi.BotAPI, msg *tgbotapi.Message) {
 		reply.Text = "Choose any games:"
 		reply.ReplyMarkup = generateHelpKeyboardForTruthOrDate()
 	default:
-		reply.Text = "..."
+		reply.Text = "I don't know the command you wrote :(\n"
+		reply.Text += "I know commands:\n"
+		reply.ReplyMarkup = generateHelpKeyboardAllCommands()
 	}
 
 	if _, err := botAPI.Send(reply); err != nil {
@@ -52,25 +61,56 @@ func handleMessage(botAPI *tgbotapi.BotAPI, msg *tgbotapi.Message) {
 }
 
 func handleCallback(botAPI *tgbotapi.BotAPI, callback *tgbotapi.CallbackQuery) {
-	msg := tgbotapi.NewMessage(callback.Message.Chat.ID, "")
+	msgMutex.Lock()
+	defer msgMutex.Unlock()
+
+	chatID := callback.Message.Chat.ID
+
+	if lastBotMsgID != 0 {
+		if _, err := botAPI.Send(tgbotapi.NewDeleteMessage(chatID, lastBotMsgID)); err != nil {
+			log.Println("Error deleting previous bot message:", err)
+		}
+	}
+
+	if _, err := botAPI.Send(tgbotapi.NewDeleteMessage(chatID, callback.Message.MessageID)); err != nil {
+		log.Println("Error deleting button message:", err)
+	}
+
+	msg := tgbotapi.NewMessage(chatID, "")
 
 	switch callback.Data {
 	case "game_truth_or_dare":
-		msg.Text = "You selected Truth or Dare!\n\n"
+		msg.Text = "You selected Truth or Dare!\n\nWhat's your choice?"
+		msg.ReplyMarkup = tgbotapi.NewInlineKeyboardMarkup(
+			tgbotapi.NewInlineKeyboardRow(
+				tgbotapi.NewInlineKeyboardButtonData("Truth", "truth"),
+				tgbotapi.NewInlineKeyboardButtonData("Dare", "dare"),
+			),
+		)
+	case "start":
+		msg.Text = generateWelcomeMessage(callback.From.FirstName)
+	case "game", "play":
+		msg.Text = "Choose a game:"
+		msg.ReplyMarkup = generateHelpKeyboardForTruthOrDate()
 	default:
-		msg.Text = "Unknown game selection"
+		msg.Text = "Unknown selection"
 	}
 
-	if _, err := botAPI.Send(msg); err != nil {
-		log.Println("Error sending callback reply: ", err)
+	sentMsg, err := botAPI.Send(msg)
+	if err != nil {
+		log.Println("Error sending reply:", err)
+	} else {
+		lastBotMsgID = sentMsg.MessageID
 	}
 
-	callbackConfig := tgbotapi.NewCallback(callback.ID, "")
-	if _, err := botAPI.Request(callbackConfig); err != nil {
-		log.Println("Error answering callback: ", err)
+	if callback.Message.ReplyToMessage != nil {
+		lastUserMsgID = callback.Message.ReplyToMessage.MessageID
+	}
+
+	if _, err := botAPI.Request(tgbotapi.NewCallback(callback.ID, "")); err != nil {
+		log.Println("Error answering callback:", err)
 	}
 }
-
 func generateWelcomeMessage(userName string) string {
 	if userName == "" {
 		userName = "friend"
@@ -83,6 +123,16 @@ func generateHelpKeyboardForTruthOrDate() tgbotapi.InlineKeyboardMarkup {
 	return tgbotapi.NewInlineKeyboardMarkup(
 		tgbotapi.NewInlineKeyboardRow(
 			tgbotapi.NewInlineKeyboardButtonData("Truth or Dare", "game_truth_or_dare"),
+		),
+	)
+}
+
+func generateHelpKeyboardAllCommands() tgbotapi.InlineKeyboardMarkup {
+	return tgbotapi.NewInlineKeyboardMarkup(
+		tgbotapi.NewInlineKeyboardRow(
+			tgbotapi.NewInlineKeyboardButtonData("/start", "start"),
+		), tgbotapi.NewInlineKeyboardRow(
+			tgbotapi.NewInlineKeyboardButtonData("/game", "game"),
 		),
 	)
 }
