87f88fb7b72f474c0797219868843cbd56eb40d7
Artem Seleznev <75618617+1Fiolet1@users.noreply.github.com>
2025-04-25 21:09:20 +0300

    Update main.go

--- cmd/bot/main.go
+++ cmd/bot/main.go
@@ -2,9 +2,10 @@ package main
 
 import (
 	"log"
-
+	
 	"github.com/ogrock3t/telegram-bot-parser/internal/bot"
 	"github.com/ogrock3t/telegram-bot-parser/internal/config"
+	"github.com/ogrock3t/telegram-bot-parser/internal/configAkar4ick.com"
 )
 
 func main() {
